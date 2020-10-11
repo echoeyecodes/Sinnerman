7945e29b4a661b35d4b9250ed9de573c797e8da0
echoeyecodes <femiobajuluwa@gmail.com>
2020-10-11 18:59:10 +0100

    added vertical video list activity

---
+++ app/src/main/java/com/example/myapplication/Activities/VideoListActivity.java
@@ -0,0 +1,28 @@
+package com.example.myapplication.Activities;
+
+import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.myapplication.R;
+
+public class VideoListActivity extends AppCompatActivity {
+private LinearLayout toolbar;
+private TextView toolbar_text;
+private ImageButton toolbar_back_btn;
+    @Override
+    protected void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_list);
+
+        toolbar = findViewById(R.id.video_list_toolbar);
+        toolbar_text = toolbar.findViewById(R.id.sub_activity_title);
+        toolbar_back_btn = toolbar.findViewById(R.id.sub_activity_back_btn);
+        String title = getIntent().getStringExtra("title");
+        toolbar_text.setText(title);
+
+        toolbar_back_btn.setOnClickListener(v -> super.onBackPressed());
+    }
+}
